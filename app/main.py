caef40bd14c8e959018e779b84393a98f36bdd90
Guilherme Holanda <142675963+Gui0206@users.noreply.github.com>
2026-03-21 13:55:15 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -13,8 +13,28 @@ class KafkaResponse:
 def main():
     server = socket.create_server(("localhost", 9092), reuse_port=True)
     connection, addr = server.accept() # wait for client
+
+    req_parse = connection.recv(1024)
+    header, client, body  = req_parse.split()
+
+    #message_size = header[:5]
+    request_api_key = header[5:7]
+    request_api_version = header[6:9]
+    correlation_id = header[9:11]
+
+    int_id = int.from_bytes(correlation_id, byteorder='big')
+
+    print('header')
+    print(header)
+    print('-----')
+    print('client')
+    print(client)
+    print('-----')
+    print('body')
+    print(body)
+
     
-    response = KafkaResponse(message_size=0,correlation_id=7)
+    response = KafkaResponse(message_size=0, correlation_id=int_id)
         
     connection.sendall(response.to_bytes())
 
