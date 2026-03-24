b8cb48fb4106719d72dca753162b038e272cad5c
Guilherme Holanda <142675963+Gui0206@users.noreply.github.com>
2026-03-24 17:37:22 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,7 +2,8 @@ import socket
 import struct
 from dataclasses import dataclass
 
-broker_supports = [0, 1, 2, 3, 4]
+supp_broker_api_versions = [0,1,2,3,4]
+
 
 @dataclass
 class KafkaResponse:
@@ -24,24 +25,14 @@ def main():
 
     print(correlation_id)
 
-    supported_api_versions = [0,1,2,3,4]
-
     request_api_version = struct.unpack(">h", data[6:8])[0]
-    print(request_api_version)
     if request_api_version not in supported_api_versions:
         error_code = 35
     else:
         error_code = 0
-
-    #error_code = struct.unpack(">i", data[])
     
     response = KafkaResponse(message_size=0, correlation_id=correlation_id, error_code=error_code)
     connection.sendall(response.to_bytes())
 
 if __name__ == "__main__":
     main()
-
-# add suport for ApiVersions version 4
-
-# add suport for the error_code field, error code 35, first 16 bit-signed
-# of the response
