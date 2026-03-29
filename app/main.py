c8e9dbc8310df4477dcc35e02db69c8b522477f7
Guilherme Holanda <142675963+Gui0206@users.noreply.github.com>
2026-03-29 14:50:00 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,22 +7,26 @@ supp_broker_api_versions = [0,1,2,3,4]
 
 @dataclass
 class KafkaResponse:
-    #message_size: int
     correlation_id: int
     error_code: int
-    request_api_key: int
     request_api_version: int
 
-
-
     def to_bytes(self):
-        #self.message_size = len(struct.pack(">hhih", self.request_api_key, self.request_api_version, self.correlation_id, self.error_code))
-        #return struct.pack(">ihhih", self.message_size, self.request_api_key, self.request_api_version, self.correlation_id, self.error_code)
+        message = b''
+        message += struct.pack(">i", self.correlation_id)
+        message += struct.pack(">h", self.error_code)
+        message += struct.pack(">h", self.request_api_version)
 
-        message = struct.pack(">hhih", self.request_api_key, self.request_api_version, self.correlation_id, self.error_code)
         self.message_size = len(message)
         return struct.pack(">i", self.message_size) + message
     
+def construct_api_arr(api_keys):
+    api_arr = b''
+    arr_len = len(api_keys)
+    api_arr += arr_len + 1
+
+    for api_key, min_version, max_version in api_keys:
+        api_arr += (api_key, min_version, max_version)
 
 def main():
     server = socket.create_server(("localhost", 9092), reuse_port=True)
@@ -33,20 +37,22 @@ def main():
 
     correlation_id = struct.unpack(">i", data[8:12])[0]
 
-    print(correlation_id)
-
     request_api_version = struct.unpack(">h", data[6:8])[0]
+
+    print(request_api_version)
 
     if request_api_version not in supp_broker_api_versions:
         error_code = 35
     else:
         error_code = 0
-        
-    response = KafkaResponse(request_api_key = 0, request_api_version = 0, correlation_id=correlation_id, error_code=error_code)
+
+    api_key = struct.unpack(">h", data[4:6])[0]
+    min_support_version = 0
+    max_support_version = 4
+    tag_buffer = 0
+
+    response = KafkaResponse(request_api_version = 0, correlation_id=correlation_id, error_code=error_code)
     connection.sendall(response.to_bytes())
 
 if __name__ == "__main__":
     main()
-
-# first test: message_size
-# message_size = numero de bytes depois do message_size
